070952b0509823e0e2c81d941b38a93051508610
Tong Jin <46266695+jinink@users.noreply.github.com>
2020-03-29 00:41:48 -0400

    Create codebook on usa-facts data
    1. descriptive analysis on confirmed cases by county provided by usa facts
    2. basic visualizations (partially)

--- data/data processing.R
+++ data/data processing.R
@@ -2,15 +2,23 @@
 # Data Processing
 
 # Setup -----
-library(data.table)
-library(dplyr)
+if(!requireNamespace("dplyr"))
+  install.packages("dplyr", repos = "https://cloud.r-project.org")
+if(!requireNamespace("data.table"))
+  install.packages("data.table", repos = "https://cloud.r-project.org")
+if(!requireNamespace("here"))
+  install.packages("here", repos = "https://cloud.r-project.org")
+require(data.table)
+require(dplyr)
+library(here)
 
 # ------------------------------------------------------------------------------
 # Data Name: covid_confirmed_usafacts.csv
-# Path: data/raw/usa-facts/03-25-2020/
+# Path: data/raw/usa-facts/03-27-2020/
 
 # Load -----
-dat <- fread("data/raw/usa-facts/03-25-2020/covid_confirmed_usafacts.csv")
+here::here()
+dat <- fread("data/raw/usa-facts/03-27-2020/covid_confirmed_usafacts_03272020.csv")
 
 # Formatted in a cumulative trend 
 head(dat, 5)
@@ -18,7 +26,4 @@ glimpse(dat)
 
 # Drop V69 column (3/26/2020)
 dat <- dat[, -69]
-
-dat <- dat %>%
-  mutate(total_confirmed = sum(3/25/2020)
 
