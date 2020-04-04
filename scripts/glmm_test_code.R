2ffcd9639be86ae7482822bbb5e9466ec2b3dca3
yinglu2017 <yingtaichi@gmail.com>
2020-04-04 08:16:05 -0400

    glmm test code and data
    glmm_test_code.R
    res and res2 run, provide good baseline fit.
    res3 with state dummies, not converged yet.
    .RDATA has basic data upto 3/29

---
+++ scripts/glmm_test_code.R
@@ -0,0 +1,103 @@
+load("daily cases in long format.RDATA")
+library(lme4)
+daily_cases$new_cases[daily_cases$new_cases<0]<-0
+ss <- table(daily_cases$state_fips) # entries per state
+cc <- table(daily_cases$county_fips) # entries per county
+sum(cc<=10)  # currently 335 counties with fewer than 5 days of entries
+
+# drop counties have fewer than 10 days of data 
+data<-daily_cases[daily_cases$county_fips %in% names(cc)[cc>10],]
+cc2<-table(data$county_fips)
+
+## county_fips=6000 is Grand Princess Cruise Ship
+data <- data[data$county_fips!="6000",]
+#tt<-table(daily_cases$county_fips)
+data$NYC <- data$county_name %in% c("Queens County",
+                                "New York County","Kings County","Bronx County")
+data$logpop <- log(data$ttl_pop)
+
+## standardize predictors is imporant to get this kind of model to converge
+## (and will probably need to orthogonalize them as well)
+
+data$day2 <- data$day^2
+data$cday<-0
+data$zday<-0
+demean <-function(x) return(x-mean(x))
+zmean <- function(x) return((x-mean(x))/var(x)^0.5)
+for (i in 1:length(cc2)) {
+  data$cday[data$county_fips==names(cc2)[i]]<-demean(data$day[data$county_fips==names(cc2)[i]])
+  data$zday[data$county_fips==names(cc2)[i]]<-zmean(data$day[data$county_fips==names(cc2)[i]])
+  }
+data$cday2<-data$cday^2
+data$zday2<-data$zday^2
+
+data$state_name<-factor(data$state_name)
+data$state_name<-relevel(data$state_name, ref="NY")
+ss.name <- names(table(data$state_name))
+state.dummies<-model.matrix(~data$state_name)
+state.zdummies<- apply(state.dummies, 2, zmean)
+state.zdummies<-as.data.frame(state.zdummies)
+names(state.zdummies)<-ss.name
+data <- cbind(data, state.zdummies)
+
+res <- glmer(new_cases~zday+zday2
+             +(zday|county_fips)+offset(logpop), data=data, 
+             family=poisson(link="log"))
+res2 <- glmer(new_cases~zday+zday2
+              +(zday+zday2|county_fips)+offset(logpop), data=data, 
+              family=poisson(link="log"))
+ff3 <- "new_cases~zday+zday2"
+for (k in 1:50) {
+  ff3 <- paste(ff3, "+",ss.name[k+1], sep="")
+}
+ff3<-paste(ff3, "++(zday+zday2|county_fips)+offset(logpop)", sep="")
+## this model still doesn't converge yet
+res3 <- glmer(formula(ff3), data=data, 
+              family=poisson(link="log"))
+
+
+summary(res)
+summary(res2)
+summary(res3) 
+# NY is the reference group
+# NJ fares out much worse than NY after accounting for tot pop
+
+pp.c <- exp(predict(res))
+pp.c2<-exp(predict(res2))
+pp.c3<-exp(predict(res3))
+#pull out county level random effect
+#rr.c<- ranef(res)
+## pull out prediction interval of these estiamtes will be time consuming
+#https://cran.r-project.org/web/packages/merTools/vignettes/Using_predictInterval.html
+
+## assess cumulative fitted values
+data$new_fit<-pp.c
+data$new_fit2<-pp.c2
+data$new_fit3<-pp.c3
+data$cum_cases <- ave(data$new_cases, data$county_fips, FUN=cumsum)
+data$cum_fit <- ave(data$new_fit, data$county_fips, FUN=cumsum)
+data$cum_fit2 <- ave(data$new_fit2, data$county_fips, FUN=cumsum)
+data$cum_fit3 <- ave(data$new_fit2, data$county_fips, FUN=cumsum)
+
+# the predicted values with or without state dummies are the same
+# but with state_dummies the prediction intervals will be narrower
+plot(data$cum_cases, data$cum_fit, pch=19, cex=0.1)
+points(data$cum_cases, data$cum_fit2, cex=0.1, col="red")
+points(data$cum_cases, data$cum_fit3, cex=0.1, col="green")
+abline(0,1)
+
+#
+AIC(res)
+AIC(res2)
+AIC(res3)
+anova(res, res2, res3)
+
+
+
+## not run
+#res4<-glmer.nb(new_cases~zday
+#         +(zday|county_fips)+offset(log(ttl_pop/1000)), data=data, 
+#         tol = 5e-3, verbose = TRUE, nb.control = NULL)
+
+# some interesting reading
+#https://rviews.rstudio.com/2017/02/01/the-r-formula-method-the-good-parts/
