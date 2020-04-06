e20df51fed27048f12e5093632ed080b30e2c431
yinglu2017 <yingtaichi@gmail.com>
2020-04-05 22:18:36 -0400

    data_processing_for_glmer_fit_or_predict

---
+++ scripts/covid_mod_data.R
@@ -0,0 +1,113 @@
+rm(list=ls())
+load("daily cases in long format.RDATA")
+data <- daily_cases
+library(lme4)
+library(dplyr)
+library(base)
+covid.mod.data.usa<-function(data, method="day", atleast_cases=NULL, days_passed=5, 
+                             which_day_start=NULL, days_to_predict=3,
+                             predict_from_whichday=max(as.Date("3/26/20", format="%m/%d/%y")),
+                             today=max(as.Date(data$date, format="%m/%d/%y")), norm.day=TRUE) {
+  
+  
+  
+  # check data
+  # make sure the first 8 items are as follows
+  # if (any(!(names(data)[1:8]) %in% c("state_fips","county_fips","state_name","county_name",
+  #                             "date","day","new_cases","cum_cases","ttl_pop"))) stop("variables names are incorrect")
+  
+  # if cumulative cases are missing, generate cumulative cases
+  
+  data <- data[data$county_name!="6000" & data$county_name!="2270",]
+  data$new_cases[data$new_cases<0 ]<-0
+  
+  if (!("cum_cases"%in% names(data))) 
+  {
+    data$cum_cases <- ave(data$new_cases, data$county_fips, FUN=cumsum)
+  }
+  
+  # convert date into R date variable
+  data$date <- as.Date(data$date, format="%m/%d/%y")
+  
+  # check if the data is up-to-date
+  if (today!=max(data$date)) stop("not all counties have up-to-date data")
+  
+  #notdo generate a data dictionary but may not needed
+  notdo <- TRUE
+  if (!notdo) {
+    # create a first day dirctionary
+    county.1stday<-data %>% group_by(county_fips) %>% summarise(date1=date[day==1],dateN=date[day==max(day)])
+  }
+  
+  # methods of data inclusion
+  if (method=="case") # based on at least # of cases 
+  { 
+    data <- data[data$cum_cases>=atleast_cases, ] 
+  }
+  else if (method=="day") # based on #days past day of 1st case
+  {
+    data <- data[data$day>=days_passed,]
+  }
+  else if (method=="whichday") # specify a minimal starting date
+  {
+    data <- data[data$date>=which_day_start,]
+  }
+  
+  ## notdo, still date dictionary
+  if (!notdo) {
+    # let the day starts from 1
+    current.1stday <- data %>% group_by(county_fips) %>% summarise(current.date1=min(day))
+    current.1stday <- merge(county.1stday, current.1stday, by="county_fips")
+  }
+  
+  # realign the day to start from 0
+  data <- data %>% group_by(county_fips) %>% mutate(new.day=as.numeric(date-min(date))) 
+  
+  # save a copy of true Y so we can compare later
+  data$new_cases_true <- data$new_cases  
+  
+  # prepare data for prediction
+  
+  if (predict_from_whichday < today)  # back-prediction is required 
+  { 
+    data$new_cases[data$date > predict_from_whichday] <-NA
+  }
+  
+  ## generate rows for predictions
+  if ((today - predict_from_whichday) < days_to_predict) # need to predict to future
+  {
+    onedaycopy <- data[data$date==today,]  ##take the last row of each county  
+    onedaycopy$new.day<-onedaycopy$new.day+1
+    onedaycopy$date<-onedaycopy$date+1
+    onedaycopy$new_cases<-NA  
+    onedaycopy$new_cases_true<-NA
+    pred.data<-onedaycopy
+    tt <-1
+    while (tt<=days_to_predict-(today-predict_from_whichday)) {
+      pred.data <- rbind(pred.data, onedaycopy)
+      onedaycopy$new.day<-onedaycopy$new.day+1
+      onedaycopy$date<-onedaycopy$date+1
+      tt <- tt+1
+    }  
+  }
+  data <- rbind(data, pred.data)
+  if (norm.day) {
+    data <- data%>% group_by(county_fips) %>% mutate(zday=(new.day-mean(new.day))/(var(new.day))^0.5,
+                                                     cday=new.day-mean(new.day))
+  }
+  data <- data%>% arrange(new.day)
+  data2$zday2 <- data2$zday^2
+  data2$logpop <- log(data2$ttl_pop)
+  data <- data %>% ungroup()
+  
+  return(data=data)
+  
+}
+
+
+today <- max(as.Date(data$date, format="%m/%d/%y"))
+data2 <- covid.mod.data.usa(data, method="day", atleast_cases=NULL, days_passed=5, 
+                            which_day_start=NULL, days_to_predict=0,
+                            predict_from_whichday=today,
+                            today=today, norm.day=TRUE)
+
--- scripts/glmm_test_code.R
+++ scripts/glmm_test_code.R
@@ -1,35 +1,43 @@
 load("daily cases in long format.RDATA")
 library(lme4)
 daily_cases$new_cases[daily_cases$new_cases<0]<-0
-ss <- table(daily_cases$state_fips) # entries per state
-cc <- table(daily_cases$county_fips) # entries per county
 sum(cc<=10)  # currently 335 counties with fewer than 5 days of entries
+data$cum_cases <- ave(data$new_cases, data$county_fips, FUN=cumsum)
 
-# drop counties have fewer than 10 days of data 
-data<-daily_cases[daily_cases$county_fips %in% names(cc)[cc>10],]
-cc2<-table(data$county_fips)
+cc2<-table(data$county_fips[data$cum_cases>10])
+
+# drop county with fewer than 10 cumulative cases.
+data <- data[data$cum_cases>10,]
+ss <- table(data$state_fips) # entries per state
+cc <- table(data$county_fips) # entries per county, 522 county
+
+
 
 ## county_fips=6000 is Grand Princess Cruise Ship
 data <- data[data$county_fips!="6000",]
 #tt<-table(daily_cases$county_fips)
-data$NYC <- data$county_name %in% c("Queens County",
-                                "New York County","Kings County","Bronx County")
+#data$NYC <- data$county_name %in% c("Queens County",
+ #                               "New York County","Kings County","Bronx County")
 data$logpop <- log(data$ttl_pop)
 
 ## standardize predictors is imporant to get this kind of model to converge
 ## (and will probably need to orthogonalize them as well)
 
-data$day2 <- data$day^2
-data$cday<-0
-data$zday<-0
-demean <-function(x) return(x-mean(x))
-zmean <- function(x) return((x-mean(x))/var(x)^0.5)
+#data$day2 <- data$day^2
+data$eday <- exp(-data$day)
+data$cday<-data$day-mean(data$day)
+data$zday<-data$cday/diff(range(data$day))
+#demean <-function(x) return(x-mean(x))
+#zmean <- function(x) return((x-mean(x))/var(x)^0.5)
 for (i in 1:length(cc2)) {
-  data$cday[data$county_fips==names(cc2)[i]]<-demean(data$day[data$county_fips==names(cc2)[i]])
-  data$zday[data$county_fips==names(cc2)[i]]<-zmean(data$day[data$county_fips==names(cc2)[i]])
+  data$day[data$county_fips==names(cc2)[i]] <- data$day[data$county_fips==names(cc2)[i]]-min(data$day[data$county_fips==names(cc2)[i]])+1 
+ # data$cday[data$county_fips==names(cc2)[i]]<-demean(data$day[data$county_fips==names(cc2)[i]])
+#  data$zday[data$county_fips==names(cc2)[i]]<-zmean(data$day[data$county_fips==names(cc2)[i]])
   }
 data$cday2<-data$cday^2
 data$zday2<-data$zday^2
+
+
 
 data$state_name<-factor(data$state_name)
 data$state_name<-relevel(data$state_name, ref="NY")
@@ -43,17 +51,18 @@ data <- cbind(data, state.zdummies)
 res <- glmer(new_cases~zday+zday2
              +(zday|county_fips)+offset(logpop), data=data, 
              family=poisson(link="log"))
-res2 <- glmer(new_cases~zday+zday2
-              +(zday+zday2|county_fips)+offset(logpop), data=data, 
-              family=poisson(link="log"))
+
+ff2<-formula("new_cases~zday+zday2+(zday+zday2|county_fips)+offset(logpop)")
+
+res2 <- update(res, ff2)
+
 ff3 <- "new_cases~zday+zday2"
 for (k in 1:50) {
   ff3 <- paste(ff3, "+",ss.name[k+1], sep="")
 }
 ff3<-paste(ff3, "++(zday+zday2|county_fips)+offset(logpop)", sep="")
 ## this model still doesn't converge yet
-res3 <- glmer(formula(ff3), data=data, 
-              family=poisson(link="log"))
+res3 <- update(res2, formula(ff3))
 
 
 summary(res)
@@ -74,7 +83,7 @@ pp.c3<-exp(predict(res3))
 data$new_fit<-pp.c
 data$new_fit2<-pp.c2
 data$new_fit3<-pp.c3
-data$cum_cases <- ave(data$new_cases, data$county_fips, FUN=cumsum)
+
 data$cum_fit <- ave(data$new_fit, data$county_fips, FUN=cumsum)
 data$cum_fit2 <- ave(data$new_fit2, data$county_fips, FUN=cumsum)
 data$cum_fit3 <- ave(data$new_fit2, data$county_fips, FUN=cumsum)
